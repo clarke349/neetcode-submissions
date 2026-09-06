4408e13ee86e52964262878db857e8d2b8794996
Malik Clarke <72239788+clarke349@users.noreply.github.com>
2026-09-06 01:48:27 -0700

    Add: two-integer-sum-ii - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        int[] output = new int[2];
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum > target) {
+                r--;
+            } else if (sum < target) {
+                l++;
+            } else {
+                output[0] = l + 1;
+                output[1] = r + 1;
+                return output;
+            }
+        }
+        return output;
+    }
+}
